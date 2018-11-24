09391d8fa19289bf8826dc2b2b3308a073d35f88
sajicode <sajioloye@gmail.com>
2018-11-25 00:27:52 +0100

    set up edit expense form and install redux devtools

--- src/app.js
+++ src/app.js
@@ -36,10 +36,10 @@ store.subscribe(() => {
 	const state = store.getState();
 	const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 
-	// console.log(visibleExpenses);
+	console.log(visibleExpenses);
 });
 
-// console.log(store.getState());
+console.log(store.getState());
 
 const jsx = (
 	<Provider store={store}>

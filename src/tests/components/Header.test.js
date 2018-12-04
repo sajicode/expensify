c70f712fd1de09fec6a68f666eb96c31955fbc7d
sajicode <sajioloye@gmail.com>
2018-12-04 15:17:04 +0100

    set up tests for components

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+// import ReactShallowRenderer from 'react-test-renderer/shallow';
 import Header from '../../components/Header';
 
+// using enzyme
 test('should render Header correctly', () => {
-	const renderer = new ReactShallowRenderer();
-	renderer.render(<Header />);
-	expect(renderer.getRenderOutput()).toMatchSnapshot();
-	// console.log(renderer.getRenderOutput());
+	const wrapper = shallow(<Header />);
+	// expect(wrapper.find('h1').text()).toBe('Expensify');
+	expect(wrapper).toMatchSnapshot();
 });
+
+// using react-test-renderer
+
+// test('should render Header correctly', () => {
+// 	const renderer = new ReactShallowRenderer();
+// 	renderer.render(<Header />);
+// 	expect(renderer.getRenderOutput()).toMatchSnapshot();
+// 	// console.log(renderer.getRenderOutput());
+// });

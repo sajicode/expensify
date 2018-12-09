60b3711868c1da7e87e79a31fe31b3b213eb2e62
sajicode <sajioloye@gmail.com>
2018-12-10 00:34:49 +0100

    separate dev dependencies from prod dependencies & change build folder

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = (env) => {
 			app: [ '@babel/polyfill', './src/app.js' ]
 		},
 		output: {
-			path: path.join(__dirname, 'public'),
+			path: path.join(__dirname, 'public', 'dist'),
 			filename: 'bundle.js'
 		},
 		target: 'web',
@@ -56,7 +56,8 @@ module.exports = (env) => {
 		devtool: isProduction ? 'source-map' : 'inline-source-map',
 		devServer: {
 			contentBase: path.join(__dirname, 'public'),
-			historyApiFallback: true
+			historyApiFallback: true,
+			publicPath: '/dist/'
 		}
 	};
 };

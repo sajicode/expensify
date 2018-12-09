f741f17a0dc280f5f57ad665c654270076263eb6
sajicode <sajioloye@gmail.com>
2018-12-09 08:36:51 +0100

    set up webpack for production

--- webpack.config.js
+++ webpack.config.js
@@ -2,35 +2,40 @@
 const devMode = process.env.NODE_ENV !== 'production';
 const path = require('path');
 
-module.exports = {
-	mode: devMode ? 'development' : 'production',
-	entry: {
-		app: [ '@babel/polyfill', './src/app.js' ]
-	},
-	output: {
-		path: path.join(__dirname, 'public'),
-		filename: 'bundle.js'
-	},
-	target: 'web',
-	module: {
-		rules: [
-			{
-				use: {
-					loader: 'babel-loader'
+module.exports = (env) => {
+	const isProduction = env === 'production';
+
+	console.log('env', env);
+	return {
+		mode: devMode ? 'development' : 'production',
+		entry: {
+			app: [ '@babel/polyfill', './src/app.js' ]
+		},
+		output: {
+			path: path.join(__dirname, 'public'),
+			filename: 'bundle.js'
+		},
+		target: 'web',
+		module: {
+			rules: [
+				{
+					use: {
+						loader: 'babel-loader'
+					},
+					test: /\.js$/,
+					include: [ path.resolve(__dirname, 'src') ],
+					exclude: /node_modules/
 				},
-				test: /\.js$/,
-				include: [ path.resolve(__dirname, 'src') ],
-				exclude: /node_modules/
-			},
-			{
-				test: /\.s?css?/,
-				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-			}
-		]
-	},
-	devtool: 'cheap-module-eval-source-map',
-	devServer: {
-		contentBase: path.join(__dirname, 'public'),
-		historyApiFallback: true
-	}
+				{
+					test: /\.s?css?/,
+					use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+				}
+			]
+		},
+		devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+		devServer: {
+			contentBase: path.join(__dirname, 'public'),
+			historyApiFallback: true
+		}
+	};
 };

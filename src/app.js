79cb803cd0584faf9fa998a527c2b139dbf0796c
sajicode <sajioloye@gmail.com>
2018-12-21 07:16:02 +0100

    add styles to components

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import { firebase } from './firebase/firebase';
+import LoadingPage from './components/LoadingPage.js';
 
 const store = configureStore();
 
@@ -26,20 +27,20 @@ const renderApp = () => {
 	}
 };
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
-firebase.auth().onAuthStateChanged((user) => {
-	if (user) {
-		store.dispatch(login(user.uid));
-		store.dispatch(startSetExpenses()).then(() => {
-			renderApp();
-			if (history.location.pathname === '/') {
-				history.push('/dashboard');
-			}
-		});
-	} else {
-		store.dispatch(logout());
-		renderApp();
-		history.push('/');
-	}
-});
+// firebase.auth().onAuthStateChanged((user) => {
+// 	if (user) {
+// 		store.dispatch(login(user.uid));
+// 		store.dispatch(startSetExpenses()).then(() => {
+// 			renderApp();
+// 			if (history.location.pathname === '/') {
+// 				history.push('/dashboard');
+// 			}
+// 		});
+// 	} else {
+// 		store.dispatch(logout());
+// 		renderApp();
+// 		history.push('/');
+// 	}
+// });
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { startLogout } from '../actions/auth';
 
 export const Header = ({ startLogout }) => (
-	<header>
-		<Link to="/dashboard">
-			<h1>Expensify</h1>
-		</Link>
-
-		<button onClick={startLogout}>Logout</button>
+	<header className="header">
+		<div className="content-container">
+			<div className="header__content">
+				<Link className="header__title" to="/dashboard">
+					<h1>Expensify</h1>
+				</Link>
+				<button className="button button--link" onClick={startLogout}>
+					Logout
+				</button>
+			</div>
+		</div>
 	</header>
 );
 

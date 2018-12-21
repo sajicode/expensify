8ad28f8d4cab45818fe2f174de83f542e1b0a835
sajicode <sajioloye@gmail.com>
2018-12-21 07:38:03 +0100

    Update test files

--- src/app.js
+++ src/app.js
@@ -29,18 +29,18 @@ const renderApp = () => {
 
 ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
-// firebase.auth().onAuthStateChanged((user) => {
-// 	if (user) {
-// 		store.dispatch(login(user.uid));
-// 		store.dispatch(startSetExpenses()).then(() => {
-// 			renderApp();
-// 			if (history.location.pathname === '/') {
-// 				history.push('/dashboard');
-// 			}
-// 		});
-// 	} else {
-// 		store.dispatch(logout());
-// 		renderApp();
-// 		history.push('/');
-// 	}
-// });
+firebase.auth().onAuthStateChanged((user) => {
+	if (user) {
+		store.dispatch(login(user.uid));
+		store.dispatch(startSetExpenses()).then(() => {
+			renderApp();
+			if (history.location.pathname === '/') {
+				history.push('/dashboard');
+			}
+		});
+	} else {
+		store.dispatch(logout());
+		renderApp();
+		history.push('/');
+	}
+});
